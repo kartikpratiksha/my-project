ae89872ea15d5d99e9d384f80c6e1b1abdfae9aa
kartikpratiksha <u.pratiksha@gmail.com>
2018-03-23 10:38:44 +0530

    added updated file

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,3 +1,65 @@
+echo "welcome to guessing game"
+function ask {
+echo "please enter the number of files in the current directory:"
+read guess
+files=$(ls -1 | wc -l)
+}
+ask
+while [[ $guess -ne $files ]]
+do
 
-this is my project
+
+if [[ $guess -lt $files ]]
+then
+echo "too low."
+else
+echo "too high."
+fi
+ask
+done
+echo "Congrats!!!. Here is the list of files:"
+
+echo "---" && ls -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
